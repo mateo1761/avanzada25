8e55ddba03f251582f7c41ad1c0cf5f901f3b393
mateo1761 <204@academicos.cesde.edu.co>
2022-03-25 08:01:30 -0500

    se crea servidor con express

--- servidorMateo/app.js
+++ servidorMateo/app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/api/v1/viernes', function (req, res) {
+  res.send('Hola mundo soy get')
+})
+
+app.post('/api/v1/viernes', function (req, res) {
+  res.send('Hola mundo soy post')
+})
+
+app.put('/api/v1/viernes', function (req, res) {
+  res.send('Hola mundo soy put')
+})
+
+app.delete('/api/v1/viernes', function (req, res) {
+  res.send('Hola mundo soy delete')
 })
 
 app.listen(3000)
