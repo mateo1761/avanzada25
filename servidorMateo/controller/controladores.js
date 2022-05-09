750b648c57f142b557c8cf37d2bbec8cff623b61
mateo1761 <mateoquinterozapata@gmail.com>
2022-05-08 20:29:51 -0500

    Se crea carpeta controller y se crean funciones

---
+++ servidorMateo/controller/controladores.js
@@ -0,0 +1,19 @@
+function buscarHabitaciones(request, response) {
+    
+}
+
+function buscarId(request, response) {
+
+}
+
+function insertar(request, response){
+
+}
+
+function editar(request, response){
+
+}
+
+function eliminar(request, response){
+
+}
